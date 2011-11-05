198a17e731ac8cff4ea450e0b002e5963ccf27ce
Adam Krebs <amk528@cs.nyu.edu>
2011-11-05 16:26:59 -0400

    initial commit

---
+++ src/net/FarmTab/app/InventoryActivity.java
@@ -0,0 +1,13 @@
+package net.FarmTab.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InventoryActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
